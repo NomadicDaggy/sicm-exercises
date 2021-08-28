6f5bd9b92a59d407363c9d775180590decd0383d
Dāgs Ādams Grīnbergs <dagsagrinbergs@gmail.com>
2021-08-28 22:44:08 +0300

    exercises up to ch 1.1.6

---
+++ ch1/1.3.scm
@@ -0,0 +1,12 @@
+(define (square a)
+    (* a a))
+
+(define (smallest a b)
+    (if (< a b) a b))
+
+(define (largest a b)
+    (if (> a b) a b))
+
+(define (largest-sum-squares a b c)
+    (+ (square (largest a b))
+       (square (largest (smallest a b) c))))
